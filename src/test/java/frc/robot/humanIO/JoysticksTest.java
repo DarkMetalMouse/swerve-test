a008abd4095cc87c84908b4731331c95339a45cd
ido <idoblibarge@gmail.com>
2021-09-25 12:45:35 +0300

    Added costume test

--- src/test/java/frc/robot/humanIO/JoysticksTest.java
+++ src/test/java/frc/robot/humanIO/JoysticksTest.java
@@ -1,6 +1,10 @@
 package frc.robot.humanIO;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import edu.wpi.first.wpilibj.Joystick;
 import frc.utils.Utils;
@@ -16,5 +20,12 @@ public class JoysticksTest {
     _joystick = (Joystick) Utils.ReflectAndSpy(_joysticks, "joystick");
   }
 
-  
+  @Test
+  public void getDesiredState(){
+
+    when(_joystick.getX()).thenReturn((double) 1.0);
+
+    assertEquals(180, _joysticks.getDesiredState().angle.getDegrees(), 0.01);
+  }
+
 }
