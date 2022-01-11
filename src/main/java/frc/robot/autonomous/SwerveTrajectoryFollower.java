6d7f3d20a6e2005e41bc7c63b9303bcdbb175d1b
Mika <mikush.carmel@gmail.com>
2022-01-11 12:44:59 +0200

    updated to 2022

--- src/main/java/frc/robot/autonomous/SwerveTrajectoryFollower.java
+++ src/main/java/frc/robot/autonomous/SwerveTrajectoryFollower.java
@@ -3,14 +3,14 @@ package frc.robot.autonomous;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
+import edu.wpi.first.math.controller.HolonomicDriveController;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.controller.HolonomicDriveController;
-import edu.wpi.first.wpilibj.controller.PIDController;
-import edu.wpi.first.wpilibj.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj.geometry.Pose2d;
-import edu.wpi.first.wpilibj.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.wpilibj.kinematics.SwerveModuleState;
-import edu.wpi.first.wpilibj.trajectory.Trajectory;
 
 public class SwerveTrajectoryFollower {
     private final Timer _timer = new Timer();
